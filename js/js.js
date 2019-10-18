ce89a84bc81fac53c53bbb0a0d4a62f72b20be2b
thepainterboy123 <thepainterboy123@gmail.com>
2019-10-17 20:23:33 -0600

    Create js.js

---
+++ js/js.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>Mens Panty Store</title>
+</head>
+<body>
+Button goes HERE
+<script>
+alert('Enter Site');
+</script>
+</body>
+</html>
